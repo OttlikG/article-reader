8936dad26851e4ee133fb60a5ce54c9f71122ee5
Gabor Ottlik <gabor.ottlik.hun@gmail.com>
2019-12-29 06:01:36 +0100

    Add selectors

---
+++ src/container/article-page/selector.ts
@@ -0,0 +1,48 @@
+import { TimelineArticle } from '../../component/ArticleTimeline/ArticleTimeline'
+
+export declare interface ArticleFromResponse {
+	source: {
+		id: string,
+		name: string,
+	},
+	author: string,
+	title: string,
+	description: string,
+	url: string,
+	urlToImage: string,
+	publishedAt: string,
+	content: string
+}
+
+export const transformMainArticle = (article: ArticleFromResponse) => {
+	return {
+		imgSrc: article.urlToImage ?? 'https://images.unsplash.com/photo-1575958951189-3442159fbadd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+		author: {
+			imgSrc: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80',
+			name: article.author,
+		},
+		articleHeading: article.title,
+		articleTime: article.publishedAt,
+		articleSummary: article.description,
+		follow: {
+			twitter: {
+				shareCount: 3,
+			},
+			facebook: {
+				shareCount: 16
+			}
+		}
+	}
+}
+
+export const transformTimelineArticles = (articles: ArticleFromResponse[]): TimelineArticle[] => {
+	return articles.map(article => {
+		return {
+			updateTime: article.publishedAt,
+			topInformationTag: article.source.name,
+			imgSrc: article.urlToImage,
+			articleHeadline: article.title,
+			articleBody: article.description
+		}
+	})
+}
